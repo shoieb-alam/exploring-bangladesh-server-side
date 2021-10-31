ffce7aeff05f78ca726faf5f4d19dfa986a8a4ae
shoieb-alam <86704871+shoieb-alam@users.noreply.github.com>
2021-10-31 18:38:14 +0600

    partial

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
+const ObjectId = require("mongodb").ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,16 +20,61 @@ async function run() {
         await client.connect();
         console.log('DB connected');
         const database = client.db("traveller");
-        const usersCollection = database.collection("users");
-        // create a document to insert
-        const doc = {
-            name: "Traveller",
-            email: "user@mongo.com",
-        }
-        const result = await usersCollection.insertOne(doc);
-        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        const servicesCollection = database.collection("users");
+        const bookingCollection = database.collection('bookings');
+
+        // GET API 
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // GET SINGLE SERVICE 
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
+        // POST API 
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('hit the post api', service);
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json(result);
+        });
+
+        // DELETE API   
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        });
+
+        // Add Booking API 
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.json(result);
+        })
+
+        // GET Bookings 
+        app.get('/mybooking', async (req, res) => {
+            const id = req.params.id;
+            // console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.find({});
+            res.json(service);
+        })
+
 
     }
+
     finally {
         // await client.close();
     }
